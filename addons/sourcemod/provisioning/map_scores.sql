1b589d615da35ffbc3b36bf905c30f418df2517f
Mithrand <mithrand001@gmail.com>
2020-07-30 15:32:20 +0200

    exampel map score file

---
+++ addons/sourcemod/provisioning/map_scores.sql
@@ -0,0 +1,6 @@
+replace into map_score (map_name, challenge, difficulty, score) values 
+('mapname1', null, 5, 1000),
+('mapname1', null, 4, 1000),
+('mapname1', 'gandalfs_revenge', 6, 1000),
+('mapname2', null, 4, 1000),
+('mapname2', null, 4, 1000);
